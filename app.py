23fc35909166e14e16b31522782cff8f4e9cfc79
uday <kuruvauday777@gmail.com>
2026-01-31 13:19:16 +0530

    Rf

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello World")
+from flask import *
+app = Flask(__name__)
+@app.route('/')
+def name():
+    return render_template('rs.html')
+if __name__ == '__main__' :
+    app.run(debug=True)
+  
